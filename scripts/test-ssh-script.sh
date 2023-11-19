0a3a6f4ee348bc69b5eac4240b08f3fec7d7e455
Timofey Sabanaev <timofey.sabanaev@qoollo.com>
2023-11-19 16:29:39 +0300

    [master] Changed field names.

--- scripts/test-ssh-script.sh
+++ scripts/test-ssh-script.sh
@@ -1,6 +1,6 @@
-username=${1:-${USERNAME}}
-hostname=${2:-${HOSTNAME}}
+deploy_username=${1:-${DEPLOY_USERNAME}}
+deploy_hostname=${2:-${DEPLOY_ADDRESS}}
 
 echo "Testing ssh connection"
 
-ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no $username@$hostname "ls"
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no $deploy_username@$deploy_hostname "ls"
